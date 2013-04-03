e4654225573cbae1863390e0dd0ac121d080b22c
agiles2 <agiles2@radford.edu>
2013-04-03 11:42:48 -0400

    first commit
    basic first two guis for the program

---
+++ newpackage/Driver.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author Alvin
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        View view = new View();
+        view.start();
+        
+    }
+}
